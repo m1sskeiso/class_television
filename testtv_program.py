18eb892e96d0ea404d7e309f76811112c3065e96
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-05-19 10:54:17 +0700

    TV 1 channels and volume

--- testtv_program.py
+++ testtv_program.py
@@ -1,10 +1,16 @@
 from tv_class import TV
 
-# Create two TV objects
+def main():
+    # Create two TV objects
+    tv1 = TV()
+    tv2 = TV()
 
-# Turn on TV1
-# Set TV1 channel to 30
-# Set TV1 volume level to 3
+    # Turn on TV1
+    tv1.turn_on()
+    # Set TV1 channel to 30
+    tv1.set_channel(30)
+    # Set TV1 volume level to 3
+    tv2.set_channel(3)
 
 # Turn on TV2
 # Set TV2 channel to 3
