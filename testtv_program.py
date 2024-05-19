2cf2a05581ef51da723be91bb5911b697bc6496e
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-05-19 10:50:28 +0700

    Pseudocode

---
+++ testtv_program.py
@@ -0,0 +1,17 @@
+from tv_class import TV
+
+# Create two TV objects
+
+# Turn on TV1
+# Set TV1 channel to 30
+# Set TV1 volume level to 3
+
+# Turn on TV2
+# Set TV2 channel to 3
+# Set TV2 volume level to 2
+
+# Print the channel and volume of TV1
+# Print the channel and volume of TV2
+
+
+
