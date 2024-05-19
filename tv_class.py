3fe66f48bd835901a181f08a7d47a68cedbc4b9f
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-05-19 08:28:41 +0700

    Pseudocode

--- tv_class.py
+++ tv_class.py
@@ -1,20 +1,30 @@
 class TV:
     def __init__(self):
+    # Initialize TV attributes
 
     def turn_on(self):
+    # Turn the TV on
 
     def turn_off(self):
+    # Turn the TV off
 
     def set_channel(self, channel):
+    # Set the TV channel if the TV is on and the channel is valid
 
     def set_volume(self, volume_level):
+    # Set the TV volume if the TV is on and the volume level is valid
 
     def channel_up(self):
+    # Increment the TV channel if the TV is on and the channel is below 120
 
     def channel_down(self):
+    # Decrement the TV channel if the TV is on and the channel is above 1
     
     def volume_up(self):
+    # Increment the TV volume if the TV is on and the volume level is below 7
 
     def volume_down(self):
+    # Decrement the TV volume if the TV is on and the volume level is above 1
+
 
     
