103b27e0b98f4452cd4b6e690dd2063bb24412a0
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-05-19 10:55:55 +0700

    tv 2 channels, and volume

--- testtv_program.py
+++ testtv_program.py
@@ -12,9 +12,12 @@ def main():
     # Set TV1 volume level to 3
     tv2.set_channel(3)
 
-# Turn on TV2
-# Set TV2 channel to 3
-# Set TV2 volume level to 2
+    # Turn on TV2
+    tv2.turn_on()
+    # Set TV2 channel to 3
+    tv1.set_channel(3)
+    # Set TV2 volume level to 2
+    tv2.set_volume(2)
 
 # Print the channel and volume of TV1
 # Print the channel and volume of TV2
