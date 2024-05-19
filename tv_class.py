ab4aefe1c51b37d47972732b55a2877033055cf3
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-05-19 08:24:01 +0700

    function for every description

--- tv_class.py
+++ tv_class.py
@@ -1,1 +1,20 @@
-# Testing 
+class TV:
+    def __init__(self):
+
+    def turn_on(self):
+
+    def turn_off(self):
+
+    def set_channel(self, channel):
+
+    def set_volume(self, volume_level):
+
+    def channel_up(self):
+
+    def channel_down(self):
+    
+    def volume_up(self):
+
+    def volume_down(self):
+
+    
