78baa313a1aeaaa8327add1f34c5712386f4e746
Marck Christian Agulto <150341453+m1sskeiso@users.noreply.github.com>
2024-05-19 08:31:41 +0700

    inserting the program to easily see

--- tv_class.py
+++ tv_class.py
@@ -1,3 +1,10 @@
+# Create Phyton Code for creating the Class named TV and a Test Driver program named 
+# TestTV that will create two objects from Class TV and will produce the following OUTPUT:
+
+# tv1's channel is 30 and volume level is 3
+# tv2's channel is 3 and volume level is 2
+
+
 class TV:
     def __init__(self):
     # Initialize TV attributes
